979652592ba1c12617f040850900e5109f24ec99
Arihant Jain <arihantj916@gmail.com>
2026-01-30 14:30:36 +0530

    feat: create schema for contactEnquiry, carreerEnquiry

---
+++ src/schema/enquiry.schema.ts
@@ -0,0 +1,26 @@
+import { lazy, z } from "zod";
+
+const contactEnquiry = z.object({
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+  email: z.email("Invalid email address"),
+  mobile: z.string().min(10).max(10),
+  companyName: z.string().min(1).max(100),
+  service: z.string().min(10).max(10),
+  message: z.string().min(30).max(500),
+  source: z.enum(["contact", "book-demo"]).optional(),
+});
+
+const carreerEnquiry = z.object({
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+  email: z.email("Invalid email address"),
+  mobile: z.string().min(10).max(10),
+  designation: z.string().min(1).max(100),
+  expInYears: z.string().min(1).max(100),
+  jobTitle: z.string().min(1).max(100),
+  resumeUrl: z.string().min(1).max(100),
+  message: z.string().min(30).max(500).optional(),
+});
+
+export { contactEnquiry, carreerEnquiry };
