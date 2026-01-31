711bf933da6fb9452fe4102dfeb35131894a886f
Arihant Jain <arihantj916@gmail.com>
2026-01-31 11:38:02 +0530

    feat: add type in schema file for admin verification

--- src/schema/otp.schema.ts
+++ src/schema/otp.schema.ts
@@ -2,12 +2,14 @@ import { z } from "zod";
 
 const sendOtpSchema = z.object({
   mobile: z.string().min(10).max(10),
+  type: z.string().optional(),
 });
 
 const verifyOtpSchema = z.object({
   mobile: z.string().min(10).max(10),
   otpId: z.string(),
   otp: z.string().min(6).max(6),
+  type: z.string().optional(),
 });
 
 export { sendOtpSchema, verifyOtpSchema };
