a734420edd128a3fbf5acfe5ab36b01bd15adb6b
Arihant Jain <arihantj916@gmail.com>
2026-01-31 15:02:01 +0530

    feat: update schema to validate blogCreate

--- src/schema/blog.schema.ts
+++ src/schema/blog.schema.ts
@@ -1,31 +1,27 @@
 import { z } from "zod";
 
-const create = z.object({
+const blogCreate = z.object({
   title: z.string().min(1).max(100),
   slug: z.string().min(1).max(100),
   shortDesc: z.string().min(50).max(200),
   content: z.string().min(100),
   category: z.string().min(5),
   tags: z.string().min(1).max(160),
-  publishedDate: z.date().optional(),
-  seo: z.object({
-    title: z.string().min(1).max(60),
-    desc: z.string().min(1).max(160),
-    keywords: z.string().min(1).max(160),
-  }),
+  publishedDate: z.coerce.date().optional(),
+
+  seo: z.preprocess(
+    (val) => (typeof val === "string" ? JSON.parse(val) : val),
+    z.object({
+      title: z.string().min(1).max(60),
+      desc: z.string().min(1).max(160),
+      keywords: z.string().min(1).max(160),
+    }),
+  ),
   status: z.enum(["draft", "published"]).default("draft"),
-  image: z
-    .instanceof(File)
-    .refine((file) => file.size > 0, "Image is required")
-    .refine(
-      (file) => ["image/jpeg", "image/png", "image/webp"].includes(file.type),
-      "Only JPG, PNG, or WEBP images are allowed",
-    )
-    .refine((file) => file.size <= 5 * 1024 * 1024, "Image must be under 5MB"),
 });
 
-const update = create.partial().extend({
+const blogUpdate = blogCreate.partial().extend({
   id: z.uuid(),
 });
 
-export { create, update };
+export { blogCreate, blogUpdate };
