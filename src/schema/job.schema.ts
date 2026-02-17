d72cd0e464b8a54e1cd6586857926781f86f86c0
Arihant Jain <arihantj916@gmail.com>
2026-02-17 15:35:42 +0530

    feat: add schema for job

---
+++ src/schema/job.schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+const createJobSchema = z.object({
+  title: z.string().min(1),
+  location: z.string().min(1),
+  type: z.string().min(1),
+  jobDesc: z.string().min(1),
+  responsibilities: z.string().min(1),
+  qualifications: z.string().min(1),
+});
+
+const updateJobSchema = createJobSchema.partial().extend({
+  id: z.uuid(),
+});
+
+export { createJobSchema, updateJobSchema };
